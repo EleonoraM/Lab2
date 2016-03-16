8545788478d8193a254397efda956b5c0a9abb9e
Eleonora <s195840@studenti.polito.it>
2016-03-16 14:05:02 +0100

    Inizio

--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/Main.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/Main.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.spellchecker.controller;
 	
+import it.polito.tdp.spellchecker.model.RichWord;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,11 +12,19 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("SpellChecker.fxml"));
+		
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("SpellChecker.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			SpellCheckerController controller = loader.getController();
+			RichWord model = new RichWord();
+			// controller.setModel (model);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,71 @@
-package it.polito.tdp.spellchecker.controller;
+	package it.polito.tdp.spellchecker.controller;
 
-public class SpellCheckerController {
-	
-}
+	import java.net.URL;
+	import java.util.ResourceBundle;
+	import javafx.event.ActionEvent;
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.ComboBox;
+	import javafx.scene.control.Label;
+	import javafx.scene.control.TextArea;
+
+	public class SpellCheckerController {
+
+			
+	    @FXML
+	    private ResourceBundle resources;
+
+	    @FXML
+	    private URL location;
+
+	    @FXML
+	    private ComboBox<?> tendinaBox;
+
+	    @FXML
+	    private TextArea testoImmesso;
+
+	    @FXML
+	    private Button btnSpell;
+
+	    @FXML
+	    private TextArea txtRestituito;
+
+	    @FXML
+	    private Label lblContieneErrori;
+
+	    @FXML
+	    private Button bntClear;
+
+	    @FXML
+	    private Label ritornaSecondi;
+
+	    @FXML
+	    void cambiaLingua(ActionEvent event) {
+
+	    	
+	    }
+
+	    @FXML
+	    void doClearText(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void doSpellCheck(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void initialize() {
+	        assert tendinaBox != null : "fx:id=\"tendinaBox\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert testoImmesso != null : "fx:id=\"testoImmesso\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert btnSpell != null : "fx:id=\"btnSpell\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert txtRestituito != null : "fx:id=\"txtRestituito\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert lblContieneErrori != null : "fx:id=\"lblContieneErrori\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert bntClear != null : "fx:id=\"bntClear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+	        assert ritornaSecondi != null : "fx:id=\"ritornaSecondi\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+	    }
+	}
+
+
